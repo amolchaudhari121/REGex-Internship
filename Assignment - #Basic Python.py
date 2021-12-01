568def7c27e3bcf1c655a68bfe5d0d46da219dee
amolchaudhari121 <88433774+amolchaudhari121@users.noreply.github.com>
2021-12-01 18:14:41 +0530

    Add files via upload

---
+++ Assignment - #Basic Python.py
@@ -0,0 +1,66 @@
+a='Amol'
+b=89
+c=8.25
+print('**************************')
+print(type(a))
+print(type(b))
+print(type(c))
+
+print('**************************')
+print(id(a))
+print(id(b))
+print(id(c))
+
+print('**************************')
+
+# Q1. Take each and every letter of your name in separate variable and try to print ASCII Value of
+# each and every letter
+# Using ord() function
+
+a1 = 'A'
+a2 = 'm'
+a3 = 'o'
+a4 = 'l'
+print('ASCII Value of given string is')
+
+print('A = ',ord(a1))
+print('m = ',ord(a2))
+print('o = ',ord(a3))
+print('l = ',ord(a4))
+
+print('**************************')
+
+# Q2 . Find the ASCII Number of word “iND@IA”
+
+b1='i'
+b2='N'
+b3="D"
+b4="@"
+b5="I"
+b6="A"
+
+print('ASCII Number of word “iND@IA” is ')
+
+print('i = ',ord(b1))
+print('N = ',ord(b2))
+print('D = ',ord(b3))
+print('@ = ',ord(b4))
+print('I = ',ord(b5))
+print('A = ',ord(b6))
+
+print('**************************')
+
+#Q3.Find the respective characters of following ASCII numbers ( use chr() )
+
+c1 = 80
+c2 = 92
+c3 = 99
+c4 = 106
+c5 = 116
+print("The characters of following ASCII numbers are..")
+
+print('88 = ',chr(c1))
+print('92 = ',chr(c2))
+print('99 = ',chr(c3))
+print('106 = ',chr(c4))
+print('116 = ',chr(c5))
---
+++ assignment_1.py
@@ -0,0 +1,11 @@
+#Assignment 1
+#arithmetic operator
+
+a = 45
+b = 35
+c = 30
+d = 90
+print('The Addition of a+b+c+d is : ',a+b+c+d)
+print('The Subtraction of a-b-c-d is : ',a-b-c-d)
+print('The Multiplication of a*b*c*d is : ',a*b*c*d)
+print('The Division of a/b/c/d is : ',a/b/c/d)
